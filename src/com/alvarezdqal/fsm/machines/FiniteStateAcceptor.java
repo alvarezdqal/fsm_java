26913c19e99507da5c78df4f1dfb6e2fa1b0c584
David Alvarez Lombardi <alvarezdqal@gmail.com>
2021-06-28 11:14:00 -0400

    broken

--- src/com/alvarezdqal/fsm/machines/FiniteStateAcceptor.java
+++ src/com/alvarezdqal/fsm/machines/FiniteStateAcceptor.java
@@ -1,3 +1,18 @@
 package com.alvarezdqal.fsm.machines;
 
-public class FiniteStateAcceptor extends FiniteStateMachine {}
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class FiniteStateAcceptor extends FiniteStateMachine {
+    HashSet<String> finalStates;
+
+    // Cant use same constructor with different args ?
+    public FiniteStateAcceptor(
+            HashSet<String> inputAlphabet,
+            HashSet<String> states,
+            String initialState,
+            HashMap<String, String> stateTransitionFunction,
+            HashSet<String> finalStates) {
+        this.finalStates = finalStates;
+    }
+}
