5ecd78bd1efffe16b83cbbb05026440c52d4ffcf
David Alvarez Lombardi <alvarezdqal@gmail.com>
2021-06-28 11:09:43 -0400

    FSM

--- src/com/alvarezdqal/fsm/machines/FiniteStateMachine.java
+++ src/com/alvarezdqal/fsm/machines/FiniteStateMachine.java
@@ -1,3 +1,22 @@
 package com.alvarezdqal.fsm.machines;
 
-public class FiniteStateMachine {}
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class FiniteStateMachine {
+    HashSet<String> inputAlphabet;
+    HashSet<String> states;
+    String initialState;
+    HashMap<String, String> stateTransitionFunction;
+
+    public FiniteStateMachine(
+            HashSet<String> inputAlphabet,
+            HashSet<String> states,
+            String initialState,
+            HashMap<String, String> stateTransitionFunction) {
+        this.inputAlphabet = inputAlphabet;
+        this.states = states;
+        this.initialState = initialState;
+        this.stateTransitionFunction = stateTransitionFunction;
+    }
+}
