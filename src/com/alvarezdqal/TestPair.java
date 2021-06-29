7a536050b31871375040479a62beb5717756382e
David Alvarez Lombardi <alvarezdqal@gmail.com>
2021-06-29 15:36:38 -0400

    ohhhh yeahhhhh

--- src/com/alvarezdqal/TestPair.java
+++ src/com/alvarezdqal/TestPair.java
@@ -4,14 +4,21 @@ import com.alvarezdqal.fsm.helpers.Pair;
 
 import java.util.HashMap;
 
-public class Test {
+public class TestPair {
     public static void main(String[] args) {
 
         Pair<String, String> firstPair = new Pair<String, String>("hello", "world");
         Pair<String, String> secondPair = new Pair<String, String>("hello", "world");
 
+        System.out.println("FIRST equals SECOND: " + firstPair.equals(secondPair));
+
+        System.out.println("FIRST HASH: " + firstPair.hashCode());
+        System.out.println("SECOND HASH: " + secondPair.hashCode());
+
         HashMap<Pair<String, String>, String> map = new HashMap<Pair<String, String>, String>();
         map.put(firstPair, "FIRST");
-        System.out.println(map.get(secondPair));
+        System.out.println("MAP[firstPair]: " + map.get(firstPair));
+        System.out.println("MAP[secondPair]: " + map.get(secondPair));
+        System.out.println("MAP CONTENTS: " + map.toString());
     }
 }
--- src/com/alvarezdqal/fsm/helpers/Pair.java
+++ src/com/alvarezdqal/fsm/helpers/Pair.java
@@ -1,17 +1,49 @@
 package com.alvarezdqal.fsm.helpers;
 
-public class Pair<X, Y> {
-    X first;
-    Y second;
+import java.util.Objects;
+
+public final class Pair<X, Y> {
+    private final X first;
+    private final Y second;
 
     public Pair(X first, Y second) {
         this.first = first;
         this.second = second;
     }
 
-    public boolean equals(Pair<X, Y> otherPair) {
-        boolean firstsMatch = this.first == otherPair.first;
-        boolean secondsMatch = this.second == otherPair.second;
-        return firstsMatch && secondsMatch;
+    public X getFirst() {
+        return this.first;
+    }
+
+    public Y getSecond() {
+        return this.second;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public boolean equals(Object obj) {
+
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null) {
+            return false;
+        }
+
+        if (this.getClass() == obj.getClass()) {
+            Pair<X, Y> otherPair = (Pair<X, Y>) obj;
+            boolean firstsMatch = this.first == otherPair.first;
+            boolean secondsMatch = this.second == otherPair.second;
+            return firstsMatch && secondsMatch;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = Objects.hash(this.first, this.second);
+        return hash;
     }
 }
